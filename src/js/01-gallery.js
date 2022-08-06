cdb05e384d3f3309bd812dadfd41f91dbf033cda
Alex-Taylor Danko <alexdankoxxl@gmail.com>
2022-08-06 19:21:55 +0300

    replace library

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -6,53 +6,21 @@ const galleryEl = document.querySelector('.gallery');
 
 galleryEl.insertAdjacentHTML('beforeend', renderGalleryItems());
 
-galleryEl.addEventListener('click', onImageClick);
-
 function renderGalleryItems() {
   return galleryItems
     .map(
       ({ original, preview, description }) =>
-        `<div class="gallery__item">
-  <a class="gallery__link" href="${original}">
-    <img
-      class="gallery__image"
-      src="${preview}"
-      data-source="${original}"
-      alt="${description}"
-    />
-  </a>
-  </div>
-`
+        `<li class="gallery__item">
+            <a class="gallery__link" href="${original}">
+             <img class="gallery__image" src="${preview}" alt="${description}" />
+            </a>
+        </li>`
     )
     .join('');
 }
 
-function onImageClick(event) {
-  if (event.target.nodeName !== 'IMG') {
-    return;
-  }
-  event.preventDefault();
-
-  const markup = `<img
-      class="gallery__image"
-      src="${event.target.dataset.source}"
-      alt="${event.target.description}"
-    />`;
-
-  const modal = SimpleLightbox.create(markup, {
-    onShow: () => {
-      addEventListener('keydown', closeOnHotKey);
-    },
-    onClose: () => {
-      removeEventListener('keydown', closeOnHotKey);
-    },
-  });
-
-  modal.show();
-
-  function closeOnHotKey(event) {
-    if (event.code === 'Escape') {
-      modal.close();
-    }
-  }
-}
+new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+  scrollZoomFactor: false,
+});
