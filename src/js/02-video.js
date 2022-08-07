d2000bdf4e6e54cb23ce8151e447ef8a6375c9d4
Alex-Taylor Danko <alexdankoxxl@gmail.com>
2022-08-07 17:51:24 +0300

    clean code

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -2,15 +2,14 @@ import Vimeo from '@vimeo/player';
 import _throttle from 'lodash.throttle';
 
 const LOCAL_STORAGE_KEY = 'videoplayer-current-time';
-const localCurrentTime = localStorage.getItem(LOCAL_STORAGE_KEY);
 
 const player = new Vimeo('vimeo-player');
 
-player.setCurrentTime(localCurrentTime);
+player.setCurrentTime(localStorage.getItem(LOCAL_STORAGE_KEY));
 
-const saveTime = _throttle(({ seconds }) => {
-  console.log(seconds);
-  localStorage.setItem(LOCAL_STORAGE_KEY, seconds);
-}, 5000);
-
-player.on('timeupdate', saveTime);
+player.on(
+  'timeupdate',
+  _throttle(({ seconds }) => {
+    localStorage.setItem(LOCAL_STORAGE_KEY, seconds);
+  }, 5000)
+);
