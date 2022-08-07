ba367f651191b725f337caf58529251015ddbe85
Alex-Taylor Danko <alexdankoxxl@gmail.com>
2022-08-07 19:37:51 +0300

    code refactor

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,35 +1,42 @@
 import _throttle from 'lodash.throttle';
-
 const formEl = document.querySelector('.feedback-form');
 const LOCAL_STORAGE_KEY = 'feedback-form-state';
 
-try {
-  let formLoad = localStorage.getItem(LOCAL_STORAGE_KEY);
-  if (formLoad) {
-    formLoad = JSON.parse(formLoad);
-    formEl.email.value = formLoad.email;
-    formEl.message.value = formLoad.message;
-  }
-} catch (error) {
-  console.error('Error.message ', error.message);
+let data = {};
+
+loadForm();
+
+formEl.addEventListener('input', _throttle(onSaveFormInput, 500));
+
+formEl.addEventListener('submit', onFormSubmit);
+
+function onSaveFormInput(event) {
+  data = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY)) || {};
+
+  data[event.target.name] = event.target.value;
+
+  localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(data));
 }
 
-formEl.addEventListener(
-  'input',
-  _throttle(() => {
-    localStorage.setItem(
-      LOCAL_STORAGE_KEY,
-      JSON.stringify({
-        email: formEl.email.value,
-        message: formEl.message.value,
-      })
-    );
-  }, 500)
-);
-
-formEl.addEventListener('submit', event => {
+function onFormSubmit(event) {
   event.preventDefault();
-  formEl.email.value = '';
-  formEl.message.value = '';
+  event.target.reset();
+
+  console.log(data);
   localStorage.removeItem(LOCAL_STORAGE_KEY);
-});
+}
+
+function loadForm() {
+  try {
+    let formLoad = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY));
+    if (!formLoad) {
+      return;
+    }
+
+    data = formLoad;
+    formEl.email.value = data.email || '';
+    formEl.message.value = data.message || '';
+  } catch (error) {
+    console.error('Error.message ', error.message);
+  }
+}
